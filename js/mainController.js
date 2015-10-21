d9fc715a5d7f0d6b245f20f58cb39cac6a3d40b7
Supreet.Singh <supreetsingh.247@gmail.com>
2015-10-21 15:53:47 +0530

    Adding Library Files

--- js/mainController.js
+++ js/mainController.js
@@ -1,3 +1,15 @@
 /**
  * Created by z001lfc on 10/21/15.
  */
+(function () {
+var MyApp = angular.module('MyApp',["ngRoute","ngMaterial","ngMessages"]);
+var mainController=function($scope,$http) {
+    $scope.link1Show=true;
+    $scope.values={
+        'conditionText':'',
+    }
+};
+MyApp.controller("mainController",["$scope","$http",mainController])
+}());
+
+
